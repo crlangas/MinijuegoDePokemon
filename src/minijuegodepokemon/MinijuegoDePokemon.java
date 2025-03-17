ca88207e4f42cb4ebe616a877910035d146dec5b
crlangas <carlospilarsanz@gmail.com>
2025-03-17 16:28:31 +0100

    añadido bucle de juego y funcion de combatir sin codigo

--- src/minijuegodepokemon/MinijuegoDePokemon.java
+++ src/minijuegodepokemon/MinijuegoDePokemon.java
@@ -12,9 +12,14 @@ import java.util.Scanner;
 public class MinijuegoDePokemon {
 
     static Scanner reader = new Scanner(System.in);
+    //creo las variables que van a tener los objetos de los pokemons contra los que jugara el jugador
+    //las creo como static para usarlas en la funciones del codigo mas adelante.
+    static Pokemon pokJugador;
+    static Pokemon pokMaquina;
 
     public static void main(String[] args) {
         // Variables
+        String pelear = "";
 
         try {
             //presentacion del juego
@@ -22,12 +27,24 @@ public class MinijuegoDePokemon {
             System.out.println("  Minijuegoo Pokemon vs Pokemon");
             System.out.println("| ----------------------------- |");
 
-            //llama a la funcion elegir pokemon para que el usuario introduzca con que pokemons quiere jugar
-            Pokemon pokJugador = elegirPokemon();
-            System.out.println("Has elegido a " + pokJugador.getNombre());
-            Pokemon pokMaquina = elegirPokemon();
-            System.out.println("Lucharas contra: " + pokMaquina.getNombre());
-            System.out.println(" Buena suerte :)");
+            pelear = "y"; //inicializo pelear en y para que el bucle se inicie
+            while (pelear.equals("y")) {
+                //llama a la funcion elegir pokemon para que el usuario introduzca con que pokemons quiere jugar
+                pokJugador = elegirPokemon();
+                System.out.println("Has elegido a " + pokJugador.getNombre());
+                pokMaquina = elegirPokemon();
+                System.out.println("Lucharas contra: " + pokMaquina.getNombre());
+                System.out.println(" Buena suerte :)");
+                
+                //comienza la pelea
+                combatir();
+                
+                // te pregunta si quieres seguir jugado
+                System.out.println("Quieres jugar otra vez? y/n");
+                
+                //guarda en pelear la primera letra en minuscula que introduzca el usuario para cortar o no el juego
+                pelear = reader.next().toLowerCase().charAt(0) + "";
+            }
         } catch (Exception e) {
             System.out.println("Error" + e);
         }
@@ -43,6 +60,7 @@ public class MinijuegoDePokemon {
             pokName = reader.next();
             pokName = pokName.toLowerCase();
 
+            //en funcion del nombre que escriba el usuario retorna un objeto u otro
             switch (pokName) {
                 case "charmander":
                     return new Pokemon("Charmander", "fuego", 90, 40);
@@ -53,11 +71,15 @@ public class MinijuegoDePokemon {
                 case "snorlax":
                     return new Pokemon("Snorlax", "normal", 200, 10);
                 default:
+                    System.out.println("Ese pokemon no esta disponible :(");
                     break;
             }
- 
 
         }
 
+    }
+
+    private static void combatir() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
