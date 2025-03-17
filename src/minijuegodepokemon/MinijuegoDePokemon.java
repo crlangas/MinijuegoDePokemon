c0f7f6fae797f85dca9cbd25faa8e1f68cfb0801
crlangas <carlospilarsanz@gmail.com>
2025-03-17 17:15:27 +0100

    ahora el usuario es capaz de hacer daño y curarse falta el enemigo

--- src/minijuegodepokemon/MinijuegoDePokemon.java
+++ src/minijuegodepokemon/MinijuegoDePokemon.java
@@ -35,13 +35,13 @@ public class MinijuegoDePokemon {
                 pokMaquina = elegirPokemon();
                 System.out.println("Lucharas contra: " + pokMaquina.getNombre());
                 System.out.println(" Buena suerte :)");
-                
+
                 //comienza la pelea
                 combatir();
-                
+
                 // te pregunta si quieres seguir jugado
                 System.out.println("Quieres jugar otra vez? y/n");
-                
+
                 //guarda en pelear la primera letra en minuscula que introduzca el usuario para cortar o no el juego
                 pelear = reader.next().toLowerCase().charAt(0) + "";
             }
@@ -80,6 +80,68 @@ public class MinijuegoDePokemon {
     }
 
     private static void combatir() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int pocionesUsadas = 0;
+        String accionPlayer = "";
+        int esquive = 0;
+        String tipoJugador = pokJugador.getTipo();
+        String tipomaquina = pokJugador.getTipo();
+        String nombrePokJugador = pokJugador.getNombre();
+
+        //bucle que sigue hasta que un pokemon muera
+        while (pokJugador.getVida() > 0 && pokMaquina.getVida() > 0) {
+
+            //bucle que pide una entrada al usuario y termina cuando lo hace bien
+            while (!accionPlayer.equals("1") && !accionPlayer.equals("2")) {
+                System.out.println("Que quieres hacer 1: atacar  o 2 curar con pocion (te quedan: " + (3 - pocionesUsadas) + ")");
+                accionPlayer = reader.next().charAt(0) + ""; // (solo coje el primer caracter que escriba el usuario)
+                if (!accionPlayer.equals("1") && !accionPlayer.equals("2")) {
+                    //mensaje motivador si el usuario se quivoca
+                    System.out.println("venga que tu puedes poner 1 o 2 eres mejor que esto :)");
+                } else {
+                    break;
+                }
+            }
+
+            //si el jugador ataca
+            if (accionPlayer.equals("1")) {
+                // hay una pequeña probabilidad de que el poquemon enemigo esquive tu ataque eso es lo que hace 
+                // esta condicion hay 1 entre 5 que se ejecute
+                esquive = (int) Math.floor(Math.random() * 5);
+                if (esquive != 0) {
+                    pokMaquina.recibirDaño(pokJugador.atacar(tipoJugador, tipomaquina), nombrePokJugador);
+
+                } else {
+                    System.out.println("vaya " + pokMaquina.getNombre() + " esquivo tu ataque");
+                }
+                //el jugador elije usar una de las 3 pociones.    
+            } else if (accionPlayer.equals("2")) {
+                pokJugador.usarPocion();
+            }
+
+            accionPlayer = "end of player play";
+
+            System.out.println(pokMaquina.getNombre() + " se prepara para atacar...");
+            try {
+                Thread.sleep(1000); // Pausa por 1 segundos
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            // inicia el turno de la maquina:
+            esquive = (int) Math.floor(Math.random() * 5);
+            if (esquive != 0) {
+                pokMaquina.recibirDaño(pokJugador.atacar(tipoJugador, tipomaquina), nombrePokJugador);
+
+            } else {
+                System.out.println("vaya " + pokMaquina.getNombre() + " esquivo tu ataque");
+            }
+
+        }
+
+        if (pokJugador.getVida() > 0) {
+            System.out.println("Felicidades ganaste");
+        } else {
+            System.out.println("Vaya perdiste contra " + pokJugador.getNombre() + "ams suerte la proxima");
+        }
+
     }
 }
